ff56eaacec5355cc5d1dc7c10d5ec696708586d7
Yubo Su <yubo@blend.com>
2022-11-07 16:10:03 -0500

    make more plots

--- plot_torque.py
+++ plot_torque.py
@@ -13,11 +13,11 @@ plt.rc('lines', lw=2.5)
 plt.rc('xtick', direction='in', top=True, bottom=True)
 plt.rc('ytick', direction='in', left=True, right=True)
 
-tf = 10
-step = 0.01
+tf = 100
+step = 0.03
 n = int(tf / step)
 
-def run(dt, dtheta_offset=np.radians(1), to_plot=True):
+def run(dt, dtheta_offset=np.radians(1), to_plot=False):
     start = time.time()
 
     sim = rebound.Simulation()
@@ -73,38 +73,6 @@ def run(dt, dtheta_offset=np.radians(1), to_plot=True):
     filename = 'test_torque_out_%.10fdt' % sim.dt
     f = open(filename + '.txt', 'w')
 
-    #############################################################
-
-    # sim.integrate(2*sim.dt)
-    # rx = ps[0].x - ps[1].x
-    # ry = ps[0].y - ps[1].y
-    # rz = ps[0].z - ps[1].z
-    # r = np.sqrt(rx**2 + ry**2 +rz**2)
-    # rx /= r
-    # ry /= r
-    # rz /= r
-    # i_dot_r = ps[1].params['tt_ix']*rx + ps[1].params['tt_iy']*ry + ps[1].params['tt_iz']*rz
-    # j_dot_r = ps[1].params['tt_jx']*rx + ps[1].params['tt_jy']*ry + ps[1].params['tt_jz']*rz
-    # f.write(str(np.arctan(j_dot_r/i_dot_r))+'\t')
-    # f.write(str(ps[1].params['tt_omega'])+'\t')
-    # f.write(str(ps[1].params['tt_ix'])+'\t')
-    # f.write(str(ps[1].params['tt_iy'])+'\t')
-    # f.write(str(ps[1].params['tt_iz'])+'\t')
-    # f.write(str(ps[1].params['tt_jx'])+'\t')
-    # f.write(str(ps[1].params['tt_jy'])+'\t')
-    # f.write(str(ps[1].params['tt_jz'])+'\t')
-    # f.write(str(ps[1].params['tt_kx'])+'\t')
-    # f.write(str(ps[1].params['tt_ky'])+'\t')
-    # f.write(str(ps[1].params['tt_kz'])+'\t')
-    # f.write(str(sim.t)+'\n')
-
-    ixprev = None
-    iyprev = None
-    izprev = None
-    jxprev = None
-    jyprev = None
-    jzprev = None
-
     times = []
     angs = []
     rxs = []
@@ -140,21 +108,26 @@ def run(dt, dtheta_offset=np.radians(1), to_plot=True):
         f.write(str(sim.t)+'\n')
         times.append(sim.t)
 
-        ixprev = ps[1].params['tt_ix']
-        iyprev = ps[1].params['tt_iy']
-        izprev = ps[1].params['tt_iz']
-        jxprev = ps[1].params['tt_jx']
-        jyprev = ps[1].params['tt_jy']
-        jzprev = ps[1].params['tt_jz']
         rxs.append(rx)
         ixs.append(ps[1].params['tt_ix'])
 
+    times = np.array(times)
     if to_plot:
-        plt.plot(times, np.unwrap(angs), 'ko')
-        # plt.ylim(np.pi - 0.2, np.pi + 0.2)
-        plt.title('%.10f' % dt)
-        plt.xlabel('Time')
-        plt.ylabel('theta')
+        fig, (ax1, ax2) = plt.subplots(
+            2, 1,
+            figsize=(8, 8),
+            sharex=True)
+
+        prefac = 3 * sim.G
+        exact_sol = np.pi - dtheta_offset * np.cos(np.sqrt(prefac / 3) * times)
+
+        ax1.plot(times, np.unwrap(angs), 'ko', label='Num')
+        ax1.plot(times, exact_sol, label='Exact')
+        ax1.set_title('%.10f' % dt)
+        ax1.set_ylabel('theta')
+        ax2.plot(times[1: ], (np.unwrap(angs) - exact_sol)[1: ])
+        ax2.set_ylabel('Residuals')
+        ax2.set_xlabel('Time')
         plt.savefig(filename + '.png', dpi=300)
         plt.clf()
 
@@ -165,7 +138,7 @@ def zero_offset_assert():
     '''
     assert that with zero offset, we get very small oscillations
     '''
-    dt = step / 2**10 # some small dt
+    dt = 1e-4 # some small dt
 
     run(dt, dtheta_offset=0, to_plot=False)
     outf = 'test_torque_out_%.10fdt.txt' % dt
@@ -179,13 +152,13 @@ def zero_offset_assert():
     angles = angles[1: ] # remove leading element
 
     assert np.min(angles) - np.max(angles) < 1e-3
-    print('Passed zero torque assertion!!')
+    print('Passed zero torque test case!!')
 
 if __name__ == '__main__':
     zero_offset_assert()
 
     dtmax = step
-    dts = dtmax / 2**np.arange(10, -1, -1)
+    dts = dtmax / 2**np.arange(8, -1, -1)
     # dts = np.array([step, step / 2])
     outfs = []
     for dt in dts:
@@ -193,7 +166,7 @@ if __name__ == '__main__':
         run(dt)
         outfs.append(outf)
 
-    nv = 3
+    nv = 15
 
     angle = 0
     omega = 1
@@ -213,6 +186,7 @@ if __name__ == '__main__':
     true_vals = np.unwrap(data[ :, angle])[1: ]
 
     rms = np.zeros(len(outfs)-1)
+    rms_an = np.zeros(len(outfs)-1)
     means = np.zeros(len(outfs)-1)
     mins = np.zeros(len(outfs)-1)
     maxes = np.zeros(len(outfs)-1)
@@ -229,7 +203,9 @@ if __name__ == '__main__':
                 data[j,k] = float(datum[k])
 
         ang = np.unwrap(data[ :, angle])[1: ]
-        rms[i-1] = np.sqrt(np.sum((ang-true_vals)**2))
+
+        rms[i-1] = np.sqrt(np.sum((ang-true_vals)**2)) / len(ang)
+        # rms[i-1] = np.abs(ang-true_vals)[-1]
         means[i-1] = np.mean(ang)
         mins[i-1] = np.min(ang[1: ])
         maxes[i-1] = np.max(ang[1: ])
@@ -238,18 +214,20 @@ if __name__ == '__main__':
         2, 1,
         figsize=(8, 8),
         sharex=True)
-    ax1.loglog(dts[1:], rms, 'go')
+    ax1.loglog(dts[1:], rms, 'go', label='RMS (data)')
     ax1.set_yscale('log')
-    ax1.plot(dts[1: ], rms[0] * (dts[1: ] / dts[1]))
+    ylims = ax1.get_ylim()
+    ax1.plot(dts[1: ], rms[3] * (dts[1: ] / dts[4]), c='r', lw=0.5,
+             label='1-order')
+    ax1.plot(dts[1: ], rms[3] * (dts[1: ] / dts[4])**2, c='y', lw=0.5,
+             label='2-order')
+    ax1.legend(fontsize=12)
+    ax1.set_ylim(ylims)
 
     ax2.plot(dts[1: ], means, 'b')
     ax2.plot(dts[1: ], mins, 'g--', lw=0.5)
     ax2.plot(dts[1: ], maxes, 'g--', lw=0.5)
     ax2.set_ylabel('Min/Mean/Max')
-
-    # ax2.plot(dts[1: ], np.abs(means - means[0]), 'go')
-    # ax2.set_ylim(bottom=0)
-    # ax2.set_ylabel('Mean - Mean0')
 
     ax1.set_ylabel('RMS (radians)')
     ax2.set_xlabel('dt (years)')
