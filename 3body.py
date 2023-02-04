1fcdcda5189a8e06a8dcd385225d66dd83804720
hdayuan <70733594+hdayuan@users.noreply.github.com>
2023-02-04 17:15:03 -0500

    try new param values

--- 3body.py
+++ 3body.py
@@ -5,11 +5,27 @@ import numpy as np
 import time
 import multiprocessing as mp
 
+def get_rand_ijk():
+    proto_i = np.array([np.random.default_rng().uniform(),np.random.default_rng().uniform(),np.random.default_rng().uniform()])
+    i = proto_i/np.sqrt(np.dot(proto_i,proto_i))
+
+    cont_bool = True
+    while (cont_bool):
+        proto_j = np.array([np.random.default_rng().uniform(),np.random.default_rng().uniform(),np.random.default_rng().uniform()])
+        pre_j = proto_j - (np.dot(proto_j,i)*i)
+        if (not np.all(pre_j == 0)):
+            cont_bool = False
+
+    j = pre_j / np.sqrt(np.dot(pre_j,pre_j))
+    k = np.cross(i,j)
+
+    return (i,j,k)
+
 # make simulation object with given parameters
 # theta = obliquity, phi = azimuthal angle, 
 # phi = 0 corresponds to initial condition where planet's k axis is tilting directly away from star
-def create_sim(sim_params,dt_frac=0.025):
-    a,Q_tide,R_p,theta,phi,omega_to_n,M_p,k2,moment2,moment3,s_k_angle,a_out,i_out,M_out = sim_params
+def create_sim(sim_params,dt_frac=0.025,rand_ijk=True):
+    a,Q_tide,R_p,theta,omega_to_n,M_p,k2,moment2,moment3,s_k_angle,a_out,i_out,M_out = sim_params
 
     sim = rebound.Simulation()
     sim.integrator = 'whfast'
@@ -25,15 +41,29 @@ def create_sim(sim_params,dt_frac=0.025):
 
     # add spin to smaller body
     ps = sim.particles
-    ps[1].params['tt_ix'] = np.cos(theta) # + ((np.sin(phi)**2) * (1-np.cos(theta)))
-    ps[1].params['tt_iy'] = 0. # -np.sin(phi)*np.cos(phi)*(1-np.cos(theta))
-    ps[1].params['tt_iz'] = -np.sin(theta)# -np.cos(phi)*np.sin(theta)
-    ps[1].params['tt_jx'] = 0. #-np.sin(phi)*np.cos(phi)*(1-np.cos(theta))
-    ps[1].params['tt_jy'] = 1. #np.cos(theta) + ((np.cos(phi)**2) * (1-np.cos(theta)))
-    ps[1].params['tt_jz'] = 0. #-np.sin(phi)*np.sin(theta)
-    ps[1].params['tt_kx'] = np.sin(theta) # *np.cos(phi)
-    ps[1].params['tt_ky'] = 0. # np.sin(theta)*np.sin(phi)
-    ps[1].params['tt_kz'] = np.cos(theta)
+
+    if not rand_ijk:
+        ps[1].params['tt_ix'] = np.cos(theta) # + ((np.sin(phi)**2) * (1-np.cos(theta)))
+        ps[1].params['tt_iy'] = 0. # -np.sin(phi)*np.cos(phi)*(1-np.cos(theta))
+        ps[1].params['tt_iz'] = -np.sin(theta)# -np.cos(phi)*np.sin(theta)
+        ps[1].params['tt_jx'] = 0. #-np.sin(phi)*np.cos(phi)*(1-np.cos(theta))
+        ps[1].params['tt_jy'] = 1. #np.cos(theta) + ((np.cos(phi)**2) * (1-np.cos(theta)))
+        ps[1].params['tt_jz'] = 0. #-np.sin(phi)*np.sin(theta)
+        ps[1].params['tt_kx'] = np.sin(theta) # *np.cos(phi)
+        ps[1].params['tt_ky'] = 0. # np.sin(theta)*np.sin(phi)
+        ps[1].params['tt_kz'] = np.cos(theta)
+
+    else:
+        i, j, k = get_rand_ijk()
+        ps[1].params['tt_ix'] = i[0]
+        ps[1].params['tt_iy'] = i[1]
+        ps[1].params['tt_iz'] = i[2]
+        ps[1].params['tt_jx'] = j[0]
+        ps[1].params['tt_jy'] = j[1]
+        ps[1].params['tt_jz'] = j[2]
+        ps[1].params['tt_kx'] = k[0]
+        ps[1].params['tt_ky'] = k[1]
+        ps[1].params['tt_kz'] = k[2]
 
     # (2/5)*MR^2
     Ii = (2/5)*M_p*R_p*R_p
@@ -73,19 +103,26 @@ def calc_orbit_normal(ps, index):
     v_hat = v_xyz / v
     return np.cross(r_hat, v_hat)
 
-# returns (obliquity, phi) of body at index index in degrees
-def get_theta_phi_deg(ps, index):
-    s_ijk = np.array([ps[index].params['tt_si'],ps[index].params['tt_sj'],ps[index].params['tt_sk']])
-    ijk_xyz = np.array([[ps[index].params['tt_ix'],ps[index].params['tt_iy'],ps[index].params['tt_iz']],[ps[index].params['tt_jx'],ps[index].params['tt_jy'],ps[index].params['tt_jz']],[ps[index].params['tt_kx'],ps[index].params['tt_ky'],ps[index].params['tt_kz']]])
+# returns (obliquity, phi) of body at index 1 in degrees
+def get_theta_phi_deg(ps):
+    # calculate theta
+    s_ijk = np.array([ps[1].params['tt_si'],ps[1].params['tt_sj'],ps[1].params['tt_sk']])
+    ijk_xyz = np.array([[ps[1].params['tt_ix'],ps[1].params['tt_iy'],ps[1].params['tt_iz']],[ps[1].params['tt_jx'],ps[1].params['tt_jy'],ps[1].params['tt_jz']],[ps[1].params['tt_kx'],ps[1].params['tt_ky'],ps[1].params['tt_kz']]])
     s_xyz = s_ijk[0]*ijk_xyz[0] + s_ijk[1]*ijk_xyz[1] + s_ijk[2]*ijk_xyz[2]
-    theta = np.degrees(np.arccos(np.dot(calc_orbit_normal(ps,index),s_xyz)))
-    # following definition of phi assumes inclination is 0
-    phi = np.degrees(np.arctan2(s_xyz[1],s_xyz[0])) 
+    n_hat = calc_orbit_normal(ps,1) # orbit normal of triaxial planet
+    theta = np.degrees(np.arccos(np.dot(n_hat,s_xyz)))
+    
+    # calculate phi
+    n_p_hat = calc_orbit_normal(ps,2) # orbit normal of perturbing planet
+    y_hat = np.cross(n_p_hat, n_hat) # unrelated to y basis unit vector
+    x_hat = np.cross(y_hat, n_hat) # unrelated to x basis unit vector
+    phi = np.degrees(np.arctan2(np.dot(s_xyz,y_hat),np.dot(s_xyz,x_hat))) 
+    
     return (theta, phi)
 
-# returns spin rate of body at index index in units of mean motion
-def get_omega_to_n(ps, index):
-    return ps[index].params['tt_omega']/ps[index].n 
+# returns spin rate of body at index 1 in units of mean motion
+def get_omega_to_n(ps):
+    return ps[1].params['tt_omega']/ps[1].n 
 
 def run_sim(trial_num, tf=5.e6, n_out=200):
 
@@ -99,7 +136,7 @@ def run_sim(trial_num, tf=5.e6, n_out=200):
     ### SIMULATION PARAMETERS ###
     # fixed parameters
     a = .4 # semi-major axis of inner planet
-    Q_tide = 10.
+    Q_tide = 100.
     R_p = 2.*Re # radius of inner planet
     M_p = 4.*Me # mass of inner planet
     k2 = 1.5 # 1.5 for uniformly distributed mass
@@ -108,24 +145,24 @@ def run_sim(trial_num, tf=5.e6, n_out=200):
     i_out = np.radians(20.) # inclination of outer planet
     M_out = Mj # mass of outer planet
     # What to do about these?
-    moment2 = 1.e-5 # 1e-1 # (Ij - Ii) / Ii, < moment3
-    moment3 = 1.e-5 # 2e-1 # (Ik - Ii) / Ii, > moment2
+    moment2 = 1.e-3 # 1e-1 # (Ij - Ii) / Ii, < moment3
+    moment3 = 1.e-3 # 2e-1 # (Ik - Ii) / Ii, > moment2
 
     # variable params
-    theta = np.pi*np.random.default_rng().uniform()
-    phi = 2*np.pi*np.random.default_rng().uniform()
-    omega_to_n = 2.*np.random.default_rng().uniform() # 2 because otherwise obliquity is excited # (1+(np.pi/2/np.arctan(1/Q_tide)))
-    
+    theta = 0. # np.pi*np.random.default_rng().uniform()
+    max_omega = 2. # 2 because otherwise obliquity is excited # (1+(np.pi/2/np.arctan(1/Q_tide)))
+    omega_to_n = max_omega*np.random.default_rng().uniform()
+
     ### RUN SIMULATION ###
     print(f"Trial {trial_num} initiated with obliquity {np.degrees(theta)} degrees and spin {omega_to_n} mean-motions.")
 
     # make sim
-    sim_params = a,Q_tide,R_p,theta,phi,omega_to_n,M_p,k2,moment2,moment3,s_k_angle,a_out,i_out,M_out
+    sim_params = a,Q_tide,R_p,theta,omega_to_n,M_p,k2,moment2,moment3,s_k_angle,a_out,i_out,M_out
     sim = create_sim(sim_params)
     ps = sim.particles
 
     # make output directory and file
-    dir_path = "./3bd_20i_1e-5j2_10Q_0.025dt"
+    dir_path = "./3bd_20i_1e-3j2_100Q_0.025dt"
     if trial_num == 0:
         if os.path.exists(dir_path):
             print("Error: Directory already exists") 
@@ -144,8 +181,8 @@ def run_sim(trial_num, tf=5.e6, n_out=200):
     for i in range(n_out):
         sim.integrate(i*step*year)
         t = sim.t / year
-        omega = get_omega_to_n(ps,1)
-        theta, phi = get_theta_phi_deg(ps,1)
+        omega = get_omega_to_n(ps)
+        theta, phi = get_theta_phi_deg(ps)
 
         f.write(str(t)+'\t')
         f.write(str(omega)+'\t')
@@ -166,4 +203,9 @@ if __name__ == '__main__':
     start = time.time()
     with mp.Pool(processes=30) as pool:
         int_times = pool.map(run_sim, range(n_trials))
-    print(f"Total Runtime: {time.time()-start}")
+    
+    tot_time = time.time() - start
+    hrs = tot_time // 3600
+    mins = (tot_time % 3600) // 60
+    secs = int((tot_time % 3600) % 60)
+    print(f"Total Runtime: {hrs} hours {mins} minutes {secs} seconds")
--- 3body_plot.py
+++ 3body_plot.py
@@ -39,11 +39,12 @@ for i in range(n_trials):
         phis[i,j] = datum[phi_ind]
 
 # plot
-fig, (ax1,ax2) = plt.subplots(2, 1,figsize=(5, 5), sharex=True)
+fig, (ax1,ax2,ax3) = plt.subplots(3, 1,figsize=(5, 8), sharex=True)
 plt.subplots_adjust(left=0.10, bottom=0.08, right=.98, top=0.98, wspace=0.05, hspace=0.02)
 ax1.set_ylabel(r"$\omega/n$")
 ax2.set_ylabel(r"$\theta$ ($^{\circ}$)")
-ax2.set_xlabel("Time (P)")
+ax3.set_ylabel(r"$\phi$ ($^{\circ}$)")
+ax3.set_xlabel("Time (P)")
 
 n_errs = 0
 for i in range(n_trials):
@@ -53,9 +54,20 @@ for i in range(n_trials):
 
     ax1.plot(ts[i],omegas[i], lw=1., color='black', alpha=0.2)
     ax2.plot(ts[i],thetas[i], lw=1., color='black', alpha=0.2)
+    ax3.plot(ts[i],phis[i], lw=1., color='black', alpha=0.2)
 
 if n_errs > 0:
     print(f"Omitting {n_errs} trials with spin rates > 2.5 n")
 
 plt.savefig('3body_trials.png', dpi=300)
+plt.clf()
+
+# plot trajectories (theta vs omega)
+fig, (ax) = plt.subplots(1, 1,figsize=(5, 5))
+plt.subplots_adjust(left=0.10, bottom=0.10, right=.98, top=0.98, wspace=0.02, hspace=0.02)
+ax.set_ylabel(r"$\theta$ ($^{\circ}$)")
+ax.set_xlabel(r"$\omega/n$")
+for i in range(n_trials):
+    ax.plot(omegas[i],thetas[i], lw=1., color='black', alpha=0.2)
+plt.savefig('3body_trajs.png', dpi=300)
 plt.clf()
